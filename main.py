f12c45c6eb1e97cf32251c51cc083652d5fe81aa
hellow0rld-lyh <69658025+hellow0rld-lyh@users.noreply.github.com>
2024-04-15 03:48:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -150,6 +150,7 @@ def get_favorite(bduss):
                     t.append(j)
         else:
             t.append(i)
+    logger.info("关注的贴吧列表: {}".format(t))
     logger.info("获取关注的贴吧结束")
     return t
 
@@ -217,13 +218,13 @@ def main():
         return
     b = ENV['BDUSS'].split('#')
     for n, i in enumerate(b):
-        logger.info("开始签到第" + str(n) + "个用户" + i)
+        logger.info("开始签到第" + str(n+1) + "个用户" + i)
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         for j in favorites:
             time.sleep(random.randint(1,5))
             client_sign(i, tbs, j["id"], j["name"])
-        logger.info("完成第" + str(n) + "个用户签到")
+        logger.info("完成第" + str(n+1) + "个用户签到")
     #send_email(favorites)
     logger.info("所有用户签到结束")
 
